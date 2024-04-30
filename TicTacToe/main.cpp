0624a9c0f5348137f93e5447516623a2363e6526
Viacheslav Yelkin <viacheslav.yelkin@gmail.com>
2024-04-30 19:32:14 +0300

    Modified main.cpp

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -9,16 +9,21 @@ int main(int argc, char** argv) {
 		TicTacToe* game = new TicTacToe();
 		game->run();
 
+	repeat_question:
 		std::cout << "Do you wish to try one more time? y/n\n";
 		char answer;
 		answer = _getch();
 
-		if (answer == 'n')
-		{
-			repeat_game = false;
-		}
-		else if (answer == 'y') {
-			continue;
+		switch (answer) {
+			case 'y':
+				continue;
+				break;
+			case 'n':
+				repeat_game = false;
+				break;
+			default:
+				goto repeat_question;
+				break;
 		}
 
 		delete game;
